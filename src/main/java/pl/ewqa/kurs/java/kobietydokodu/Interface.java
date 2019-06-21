e6ec8609270e77dcf17c4dd8d7478983a8037b4a
elciak82 <elciak82@gmail.com>
2019-06-21 11:24:02 +0200

    #4 Kobiety do kodu

--- src/main/java/pl/ewqa/kurs/java/kobietydokodu/Interface.java
+++ src/main/java/pl/ewqa/kurs/java/kobietydokodu/Interface.java
@@ -24,7 +24,7 @@ public class Interface {
         } while (kot.getBornDate() == null);
 
         do {
-            System.out.println("Podaj wagę Kotka w kilogranach.");
+            System.out.println("Podaj wagę Kotka w kilogramach.");
             String weightAsString = getUserInput();
             kot.setWeight(parseStringToFloat(weightAsString));
         } while (kot.getWeight() == null);
@@ -38,27 +38,32 @@ public class Interface {
         } //pobieranie wartości, którą wpisze user
 
         private static Date parseStringToDate(String dateFormat, String dateAsString) {
-            Pattern pattern = Pattern.compile("yyyy-mm-dd"); //TODO popraw lekcja 4
-            Matcher matcher = pattern.matcher(dateAsString); //TODO popraw lekcja 4
+            Pattern pattern = Pattern.compile("^((?:19|20)\\d\\d)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$");
+            Matcher matcher = pattern.matcher(dateAsString);
             SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
             Date date = null;
-            if (matcher.matches()) { //TODO popraw lekcja 4
+            if (matcher.matches()) {
                 try {
                     date = sdf.parse(dateAsString);
                 } catch (ParseException e) {
-                    System.out.println("Format daty jest nieprawidłowy. Przykładowy format daty: 2019.04.17.");
+                    System.out.println("Format daty jest nieprawidłowy. Przykładowy format daty: '2019.04.17'.");
                 }
-            } else
-                System.out.println("dupa"); //TODO popraw lekcja 4
+            }
+            else
+                System.out.println("Jesteż pewny, że to prawidłowa data? Spróbuj jeszcze raz!. Przykładowy format daty: '2019.04.17'.");
             return date;
         }
         
         private static Float parseStringToFloat(String weightAsString) {
+            Pattern pattern = Pattern.compile("[0-9]+(\\.[0-9]+)?");
+            Matcher matcher = pattern.matcher(weightAsString);
             Float weight = null;
-            try {
-                weight = Float.parseFloat(weightAsString);
-            }catch (NumberFormatException e) {
-                System.out.println("Format wagi jest nieprawidłowy. Przykładowy format wagi: 5.6.");
+            if (matcher.matches()) {
+                try {
+                    weight = Float.parseFloat(weightAsString);
+                } catch (NumberFormatException e) {
+                    System.out.println("Format wagi jest nieprawidłowy. Przykładowy format wagi: 5.6.");
+                }
             }
             return weight;
         }   
